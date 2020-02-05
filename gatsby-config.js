06ad4c07b15d4ebe164248a7f52bb7b7b61059b0
Floris Kappen <kappenfloris@gmail.com>
2020-02-05 11:09:53 +0100

    Make it a PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,23 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Floris Portfolio`,
+        short_name: `FPort`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    }
   ],
 }
